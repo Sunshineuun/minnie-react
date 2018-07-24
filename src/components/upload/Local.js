cdbe6c6266cfcd67be76130bcc18180f8be1f467
qiushengming@aliyun.com <sunshine>
2018-07-24 17:07:00 +0800

    上传功能

--- src/components/upload/Local.js
+++ src/components/upload/Local.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import { Upload, Icon, message } from 'antd';
+import {Upload, Icon, message} from 'antd';
+
 const Dragger = Upload.Dragger;
-const LOCAL_SERVER = "http://localhost:8080/upload";
+const LOCAL_SERVER = "http://localhost:8085/importData/importFile";
 
 const props = {
   name: 'file',
   listType: 'picture',
   multiple: true,
   action: LOCAL_SERVER,
-  onChange(info) {
-    const status = info.file.status;
+  data: {"test": "test"},
+  onChange({file, fileList, event}) {
+    const status = file.status;
+    // 服务器段传回来的响应结果
+    let success = false;
+
     if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
+      console.log(file, fileList);
+      success = file.response.success;
+    }else{
+      if(fileList.length > 3){
+        fileList.splice(0, 3);
+      }
     }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
+
+    if (status === 'done' && success) {
+      message.success(`${file.name} 文件上传成功.`);
+    } else if ((status === 'error' || status === 'done')
+      && success === false) {
+      message.error(`${file.name} 文件上传失败, ${file.response.msg}.`);
     }
   },
 };
 
-export default class Local extends React.Component{
+export default class Local extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
@@ -31,7 +43,7 @@ export default class Local extends React.Component{
       <div>
         <Dragger {...props}>
           <p className="ant-upload-drag-icon">
-            <Icon type="inbox" />
+            <Icon type="inbox"/>
           </p>
           <p className="ant-upload-text">点击或拖拽上传</p>
           <p className="ant-upload-hint">本地文件服务</p>
